5366cf6e13593eca0775d6e3e537f4d024373512
Joris VILARDELL <73846641+jorisvilardell@users.noreply.github.com>
2026-01-12 14:37:47 +0100

    fix: only validate client secret for non-public clients (#657)
    * Fix: only validate client secret for non-public clients
    
    * Fix: Simplify client secret validation logic

--- core/src/domain/authentication/services.rs
+++ core/src/domain/authentication/services.rs
@@ -399,12 +399,14 @@ where
             .await
             .map_err(|_| CoreError::InvalidClient)?;
 
-        if !client.direct_access_grants_enabled {
-            if params.client_secret.is_none() {
+        if !client.public_client {
+            if !client.direct_access_grants_enabled && params.client_secret.is_none() {
                 return Err(CoreError::InvalidClientSecret);
             }
 
-            if client.secret != params.client_secret {
+            if let Some(provided_secret) = params.client_secret
+                && client.secret != Some(provided_secret)
+            {
                 return Err(CoreError::InvalidClientSecret);
             }
         }
